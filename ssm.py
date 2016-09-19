924ede77ca7e6f5156c7b1cb58be0ef56ce1af84
Felipe Felix <felipescript@gmail.com>
2016-09-19 18:50:49 -0300

    Testing ssm with chroma feature.

--- ssm.py
+++ ssm.py
@@ -1,22 +1,21 @@
 import numpy as np
+import matplotlib
 
 class ssm:
-
     def __init__(self, feat):
-        self.s = create_ssm(feat)
+        self.s = self.create_ssm(feat)
 
-    def create_ssm(feat):
+    def create_ssm(self, feat):
         M, N = feat.shape
-        s = np.zeros(M * N).reshape(M, N)
-        for i in range(M):
+        print(M, N)
+        s = np.zeros(N * N).reshape(N, N)
+        for i in range(N):
             for j in range(N):
-                s[i, j] = self.dist(feat[i], feat[j])
+                s[i, j] = self.dist(feat[:, i], feat[:, j])
         return s
 
-    def dist(f, g):
+    def dist(self, f, g):
         return np.dot(f, g)
 
-    def visualize(s):
-        pass
-
-
+    def visualize(self):
+        matplotlib.pcolor(self.s)
